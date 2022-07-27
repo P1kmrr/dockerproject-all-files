0c89fa54b2e156f77b3353725fc5d243c4027f43
P1kmrr <7993612171pk@gmail.com>
2022-07-27 13:17:48 +0530

    Initial commit
    Docker files

---
+++ 09_pythonproj/pythn.py
@@ -0,0 +1,13 @@
+# Print Hello User
+print("Hello User")
+
+# assigning value to var b
+b = 2022
+
+# Assigning logic to var c
+c = int(input("Enter your year of birth "))
+
+# 
+a = (b-c)
+print("Your age is ",a)
+
